b81b39cc20d46f15dd95e08bc746d6a98d179d4d
Stephen <stephen@mott.email>
2026-05-30 21:26:36 +0800

    fix(bash-lsp): add shfmt check

--- test/bash-language-server/non-root-user.sh
+++ test/bash-language-server/non-root-user.sh
@@ -2,6 +2,7 @@
 set -e
 source dev-container-features-test-lib
 
+check "not running as root" bash -c "[ \"$(id -u)\" != '0' ]"
 check "bash-language-server is on PATH" which bash-language-server
 check "bash-language-server --version exits 0" bash-language-server --version
 check "shellcheck is on PATH" which shellcheck
